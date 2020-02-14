a542a0c9ae590b8f765ac7f792a517a8cd63b3cd
SWH <slgo123@naver.com>
2020-02-14 17:43:35 +0900

    20200214

--- 2020020301.js
+++ 2020020301.js
@@ -1,35 +1,14 @@
 const title = document.querySelector("#title");
 
-console.dir(title);
+const CLICKED_CLASS = "clicked";
 
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "#7f8c8d";
-
-function handler(){
-    const Tcolor = title.style.color;
-    console.log(Tcolor);
-    if(Tcolor === BASE_COLOR){
-        title.style.color = OTHER_COLOR;
-    }
-    else{
-        title.style.color = BASE_COLOR;
-    }
+function handleClick(){
+    title.classList.toggle(CLICKED_CLASS);
 }
 
 function init(){
-    title.style.color = BASE_COLOR;
-    title.addEventListener("mouseenter",handler);
+    title.addEventListener("click",handleClick);
 }
 
 init();
 
-function handoff(){
-    console.log("인터넷꺼짐!");
-}
-
-function handon(){
-    console.log("인터넷연결됨!");
-}
-
-window.addEventListener("offline",handoff);
-window.addEventListener("online",handon);
